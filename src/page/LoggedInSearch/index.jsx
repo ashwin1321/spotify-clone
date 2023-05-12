733adf3fd5f3ea94c6fddba8cedd07f94cdbe986
ashwin1321 <ashwinkhatiwada7@gmail.com>
2023-05-12 16:57:25 +0545

    feat: some changes

--- src/page/LoggedInSearch/index.jsx
+++ src/page/LoggedInSearch/index.jsx
@@ -5,9 +5,17 @@ import { Container, Row, Col, Card, CardBody, CardText } from 'reactstrap'
 import SearchPage from '../SearchPage'
 import { BsFillPlayFill } from "react-icons/bs";
 import { songs } from '../../assets/data/spotify'
+import { Link } from 'react-router-dom'
+import SongCard from '../../components/Card/SongCard'
 
 const index = () => {
 
+    const cardData = [
+        { id: 1, imageUrl: "https://i.scdn.co/image/ab67616100005174d42a27db3286b58553da8858", title: "Dua Lipa", subtitle: "Artist" },
+        { id: 2, imageUrl: "https://i.scdn.co/image/ab676161000051749e690225ad4445530612ccc9", title: "Ed Sheeran", subtitle: "Artist" },
+        { id: 3, imageUrl: "https://i.scdn.co/image/ab676161000051745a00969a4698c3132a15fbb0", title: "Taylor Swift", subtitle: "Artist" },
+        { id: 4, imageUrl: "https://i.scdn.co/image/ab67616100005174cdce7620dc940db079bf4952", title: "Ariana Grande", subtitle: "Artist" },
+    ];
 
     function shuffleArray(array) {
         const shuffledArray = [...array]; // Make a copy of the original array
@@ -35,6 +43,9 @@ const index = () => {
     });
 
     const shuffleSongsList = shuffleArray(songsList);
+    const shuffleSongsList1 = shuffleArray(songsList);
+    const shuffleSongsList2 = shuffleArray(songsList);
+    const shuffleSongsList3 = shuffleArray(songsList);
 
     return (
         <>
@@ -81,7 +92,7 @@ const index = () => {
                             <h2 className="fw-bold fs-4 text-white">Songs</h2>
                         </div>
 
-                        {shuffleSongsList.slice(0, 4).map((song, i) => (
+                        {shuffleSongsList3.slice(0, 4).map((song, i) => (
 
                             <div key={i} className='m-0 ms-0 mt-2 d-flex table-roww p-2 align-items-center' >
                                 <div className="col-9">
@@ -106,7 +117,41 @@ const index = () => {
 
             </Container>
 
-            <SearchPage />
+            <Container fluid className="p-0 my-2 ms-3 mb-2 contd ">
+                {renderSections("Featuring Dua Lipa", shuffleSongsList.slice(0, 4))}
+            </Container>
+
+            <Container fluid className="p-0 my-2 ms-3 contd my-3">
+                <div className="d-flex justify-content-between align-items-end mt-4">
+                    <h2 className="fw-bold fs-4 mt-3 text-white">Artists</h2>
+                </div>
+
+                <div className=" p-0 border-5 d-flex gap-4 mt-3">
+                    {cardData.map((card) => (
+                        <Card className="cardComp cardsssa border-0 rounded-3 gap-3" key={card.id}>
+                            <div className="position-relative">
+                                <img src={card.imageUrl} alt="..." className="imgg2 rounded-circle" />
+                            </div>
+                            <CardBody className="m-0 p-0">
+                                <CardText className="m-0 fw-Semibold fs-4 cardtitle text-white mbody">{card.title}</CardText>
+                                <CardText className="m-0 fs-6 text-secondary mbody">{card.subtitle}</CardText>
+                            </CardBody>
+                            <Button className="playButtons rounded-circle border-0 d-flex justify-content-center align-items-center p-2 mb-3 me-2 shadow">
+                                <BsFillPlayFill fill="black" size={30} />
+                            </Button>
+                        </Card>
+                    ))}
+                </div>
+
+            </Container>
+
+            <Container fluid className="p-0 my-2  mb-2 contd">
+                {renderSections("Playlists", shuffleSongsList1.slice(0, 7))}
+            </Container>
+
+            <Container fluid className="p-0 my-2 mt-3 mb-2 contd ">
+                {renderSections("Podcasts", shuffleSongsList2.slice(0, 7))}
+            </Container>
         </>
 
 
@@ -114,3 +159,27 @@ const index = () => {
 }
 
 export default index
+
+
+
+const renderSections = (sectionTitle, songs) => {
+    return (
+
+        <div>
+            <div className="d-flex justify-content-between align-items-end">
+                <h2 className="fw-bold fs-4 text-white mt-3">{sectionTitle}</h2>
+            </div>
+            <div className="mt-3 cards gap-4 px-1">
+                {songs.map((song, i) => (
+                    <SongCard
+                        key={i}
+                        id={i}
+                        imgSrc={song.imgTags}
+                        title={song.title}
+                        subtitle={song.artist}
+                    />
+                ))}
+            </div>
+        </div>
+    )
+};
